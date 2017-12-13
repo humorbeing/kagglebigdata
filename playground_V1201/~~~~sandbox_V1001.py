28d317bb963ad771acab2f191e4f300feaf2ae27
ray <geemguang@gmail.com>
2017-12-13 14:04:44 +0900

    real train error

--- playground_V1201/~~~~sandbox_V1001.py
+++ playground_V1201/~~~~sandbox_V1001.py
@@ -1,5 +1,9 @@
-a = [i for i in range(100)]
-
-b = a[80::3]
-print(a)
-print(b)
+# a = [i for i in range(100)]
+#
+# b = a[80::3]
+# print(a)
+# print(b)
+import shelve
+d = shelve.open('f')
+import dbm
+print(dbm.whichdb('f'))
